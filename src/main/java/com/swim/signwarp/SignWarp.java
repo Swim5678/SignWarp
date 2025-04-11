7be4fa88698f8d63593ac20bc2c20d14c2ef513c
swim5678 <154345558+verdo568@users.noreply.github.com>
2025-04-11 18:18:22 +0800

    remove UpdateChecker.java

--- src/main/java/com/swim/signwarp/SignWarp.java
+++ src/main/java/com/swim/signwarp/SignWarp.java
@@ -11,14 +11,6 @@ public final class SignWarp extends JavaPlugin implements Listener {
     private static final String PLUGIN_URL = "https://www.spigotmc.org/resources/signwarp-teleport-using-the-signs." + RESOURCE_ID + "/";
 
     public void onEnable() {
-        // Check for updates
-        new UpdateChecker(this, RESOURCE_ID).getVersion(version -> {
-            if (this.getDescription().getVersion().equals(version)) {
-                getLogger().info("沒有可用的新版本");
-            } else {
-                getLogger().warning("該插件的新版本現已推出： " + version + " （目前的： " + this.getDescription().getVersion() + "）。在這裡下載： " + PLUGIN_URL);
-            }
-        });
 
         // Save default config
         saveDefaultConfig();
@@ -43,25 +35,6 @@ public final class SignWarp extends JavaPlugin implements Listener {
         pluginManager.registerEvents(new WarpGuiListener(this), this);
         pluginManager.registerEvents(this, this);
     }
-
-//    @EventHandler
-//    public void onPlayerJoin(PlayerJoinEvent event) {
-//        Player player = event.getPlayer();
-//
-//        if (player.isOp()) {
-//        new UpdateChecker(this, RESOURCE_ID).getVersion(version -> {
-//            if (!this.getDescription().getVersion().equals(version)) {
-//                player.sendMessage(
-//                        ChatColor.DARK_RED + "⚠ A new version of SignWarp is available: " +
-//                                ChatColor.RED + version +
-//                                " (current: " + this.getDescription().getVersion() + "). " +
-//                                ChatColor.DARK_RED + "Download it here: " + ChatColor.RED + PLUGIN_URL
-//                );
-//                }
-//            });
-//        }
-//    } for the soon release
-
     @Override
     public void onDisable() {
         // Plugin shutdown logic
