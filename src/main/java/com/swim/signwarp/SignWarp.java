7f95d0f285044ca797ff869c507b37a9d99b18cc
swim5678 <154345558+verdo568@users.noreply.github.com>
2025-04-15 08:45:48 +0800

    Update SignWarp.java

--- src/main/java/com/swim/signwarp/SignWarp.java
+++ src/main/java/com/swim/signwarp/SignWarp.java
@@ -8,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class SignWarp extends JavaPlugin implements Listener {
 
     public void onEnable() {
-
+//test 
         // Save default config
         saveDefaultConfig();
 
