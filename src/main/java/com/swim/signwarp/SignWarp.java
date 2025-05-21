9794354d656374c0295982f017ada00e7b884180
swim5678 <154345558+verdo568@users.noreply.github.com>
2025-05-21 19:52:50 +0800

    新增對伺服器版本的檢查，僅支援 Paper 伺服器版本 1.21.4

--- src/main/java/com/swim/signwarp/SignWarp.java
+++ src/main/java/com/swim/signwarp/SignWarp.java
@@ -8,28 +8,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class SignWarp extends JavaPlugin implements Listener {
 
     public void onEnable() {
-        // Save default config
-        saveDefaultConfig();
-
-
-        // Initialize database and migrate table if needed
-        Warp.createTable();
-        Warp.createInvitesTable();// Create the invites table if it doesn't exist
-        // Register commands and tab completer
-        PluginCommand command = getCommand("signwarp");
-        if (command != null) {
-            SWCommand swCommand = new SWCommand(this);
-            command.setExecutor(swCommand);
-            command.setTabCompleter(swCommand);
+        String bukkitVersion = getServer().getBukkitVersion();
+        if (!bukkitVersion.startsWith("1.21.4")) {
+            getLogger().severe("此插件僅支援 Paper 伺服器版本 1.21.4，當前版本: "
+                    + bukkitVersion + "，即將停用插件");
+            getServer().getPluginManager().disablePlugin(this);
         } else {
-            getLogger().warning("Command 'signwarp' not found!");
+            // Save default config
+            saveDefaultConfig();
+
+
+            // Initialize database and migrate table if needed
+            Warp.createTable();
+            Warp.createInvitesTable();// Create the invites table if it doesn't exist
+            // Register commands and tab completer
+            PluginCommand command = getCommand("signwarp");
+            if (command != null) {
+                SWCommand swCommand = new SWCommand(this);
+                command.setExecutor(swCommand);
+                command.setTabCompleter(swCommand);
+            } else {
+                getLogger().warning("Command 'signwarp' not found!");
+            }
+
+            // Register event listener
+            PluginManager pluginManager = getServer().getPluginManager();
+            pluginManager.registerEvents(new EventListener(this), this);
+            pluginManager.registerEvents(new WarpGuiListener(this), this);
+            pluginManager.registerEvents(this, this);
         }
 
-        // Register event listener
-        PluginManager pluginManager = getServer().getPluginManager();
-        pluginManager.registerEvents(new EventListener(this), this);
-        pluginManager.registerEvents(new WarpGuiListener(this), this);
-        pluginManager.registerEvents(this, this);
     }
     @Override
     public void onDisable() {
